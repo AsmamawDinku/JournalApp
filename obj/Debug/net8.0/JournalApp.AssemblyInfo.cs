ff26af23bef0f153bfefe3c4ec45be698c811f37
AsmamawDinku <adinku@byupathway.edu>
2025-05-06 22:40:21 -0400

    re commit

--- obj/Debug/net8.0/JournalApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/JournalApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JournalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f7de1181e88434271c639dd34f5b95ae101302")]
 [assembly: System.Reflection.AssemblyProductAttribute("JournalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JournalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
